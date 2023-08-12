5175aeec9ccba2a7fa21235043f01a152087746e
tatyana86 <Krivonogova_1999@mail.ru>
2023-08-12 16:09:30 +0400

    add solution v1

---
+++ task3/DynArray.java
@@ -0,0 +1,91 @@
+import java.lang.reflect.Array;
+
+public class DynArray<T>
+{
+    public T [] array;
+    public int count;
+    public int capacity;
+    private static final int minCapacity = 16;
+    private static final double minFullness = 0.5;
+    Class clazz;
+
+    public DynArray(Class clz)
+    {
+        clazz = clz; 
+
+        count = 0;
+        makeArray(16);
+    }
+
+    public void makeArray(int new_capacity)
+    {
+        if(new_capacity < minCapacity) {
+            this.capacity = minCapacity;
+        }
+
+        T [] tempArray = (T[]) Array.newInstance(this.clazz, new_capacity);
+
+        if(this.count > 0) {
+            System.arraycopy(array, 0, tempArray, 0, this.count);
+        }
+
+        this.array = tempArray;
+        this.capacity = new_capacity;
+    }
+
+    public T getItem(int index)
+    {
+        isExistIndex(index);
+        return array[index];
+    }
+
+    public void append(T itm)
+    {
+        if(this.count == this.capacity) {
+            makeArray(2 * this.capacity);
+        }
+        array[this.count] = itm;
+        this.count ++;
+    }
+
+    public void insert(T itm, int index)
+    {
+        isCorrectIndex(index);
+
+        if(index == count) {
+            append(itm);
+        }
+
+        if(this.count == this.capacity) {
+            makeArray(2 * this.capacity);
+        }
+
+        System.arraycopy(this.array, index, this.array, index + 1, this.count - index);
+        this.array[index] = itm;
+        this.count ++;
+    }
+
+    public void remove(int index)
+    {
+        isExistIndex(index);
+        System.arraycopy(this.array, index, this.array, index - 1, this.count - index);
+        this.count --;
+
+        if(this.count < (this.capacity * minFullness)) {
+            makeArray((int) (this.capacity / 1.5));
+        }
+    }
+
+    private void isExistIndex(int index) {
+        if(index < 0 || index >= count) {
+            throw new ArrayIndexOutOfBoundsException("Incorrect index!");
+        }
+    }
+
+    private void isCorrectIndex(int index) {
+        if(index < 0 || index > count) {
+            throw new ArrayIndexOutOfBoundsException("Incorrect index!");
+        }
+    }
+
+}
---
+++ task3/DynArrayTest.java
@@ -0,0 +1,101 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DynArrayTest {
+
+    private static final int minCapacity = 16;
+    private DynArray<Integer> emptyArray = new DynArray<Integer>(Integer.class);
+
+    @Test
+    public void insertElemWhenCountLessThenCapacity() {
+
+        emptyArray.append(1);
+        emptyArray.insert(2, 0);
+
+        assertEquals(minCapacity, emptyArray.capacity);
+        assertEquals(2, emptyArray.count);
+        assertEquals(1, emptyArray.getItem(1));
+        assertEquals(2, emptyArray.getItem(0));
+    }
+
+    @Test
+    public void insertElemWhenCountEqualCapacity() {
+
+        for (int i = 0; i < minCapacity; i ++) {
+            emptyArray.append(i);
+        }
+
+        emptyArray.insert(2, 5);
+
+        assertEquals(minCapacity * 2, emptyArray.capacity);
+        assertEquals(17, emptyArray.count);
+        assertEquals(15, emptyArray.getItem(16));
+    }
+
+    @Test
+    public void insertElemWithIncorrectIndex() {
+
+        assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> emptyArray.insert(2, -1));
+        assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> emptyArray.insert(2, 1));
+
+    }
+
+    @Test
+    public void insertElemsWithIncreaseCapacityTwice() {
+
+        for (int i = 0; i < 35; i++) {
+            emptyArray.insert(i, i);
+        }
+
+        assertEquals(minCapacity * 2 * 2, emptyArray.capacity);
+
+    }
+
+    @Test
+    public void removeWhenCapacityIsSame() {
+
+        emptyArray.makeArray(40);
+
+        for (int i = 0; i < 21; i ++) {
+            emptyArray.append(i);
+        }
+
+        emptyArray.remove(10);
+
+        assertEquals(40, emptyArray.capacity);
+        assertEquals(20, emptyArray.count);
+        assertEquals(11, emptyArray.getItem(10));
+    }
+
+    @Test
+    public void removeWhenCapacityShoulReduce() {
+
+        emptyArray.makeArray(40);
+
+        for (int i = 0; i < 20; i ++) {
+            emptyArray.append(i);
+        }
+
+        emptyArray.remove(10);
+
+        assertEquals(26, emptyArray.capacity);
+        assertEquals(19, emptyArray.count);
+        assertEquals(11, emptyArray.getItem(10));
+    }
+
+    @Test
+    public void removeElemWithIncorrectIndex() {
+
+        for (int i = 0; i < 20; i ++) {
+            emptyArray.append(i);
+        }
+
+        assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> emptyArray.remove(-1));
+        assertThrowsExactly(ArrayIndexOutOfBoundsException.class, () -> emptyArray.remove(21));
+
+        emptyArray.remove(3);
+        assertEquals(4, emptyArray.getItem(3));
+
+    }
+}
